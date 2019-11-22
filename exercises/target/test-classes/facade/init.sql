aa13c7120b541c1df88bb82d75d9a7a9521c2e28
vicziani <viczian.istvan@gmail.com>
2019-11-22 16:58:23 +0100

    Törlés

--- exercises/target/test-classes/facade/init.sql
+++
@@ -1,4 +0,0 @@
-create table locations(id bigint auto_increment, name varchar(255), lat double, lon double);
-insert into locations(name, lat, lon) values ('Budapest', 47.4979, 19.0402);
-insert into locations(name, lat, lon) values ('Békéscsaba', 46.673594, 21.087731);
-insert into locations(name, lat, lon) values ('Szeged', 46.253, 20.1414);
